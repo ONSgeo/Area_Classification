7058ead16bc6131af71e76648f303e0ade00d644
Parker <sarah.parker@ons.gov.uk>
2025-04-10 11:23:44 +0100

    Fudging code to get it to work, and changing OA -> LTLA

--- Downloading_data/EW_Census_2021_Output_Areas-download/bulk_data.R
+++ Downloading_data/EW_Census_2021_Output_Areas-download/bulk_data.R
@@ -3,7 +3,7 @@ library(vroom)
 library(stringr)
 library(tidyverse)
 library(magrittr)
-library(arrow)
+#library(arrow)
 
 # Read the HTML page
 html_page <- read_html("https://www.nomisweb.co.uk/sources/census_2021_bulk")
@@ -65,59 +65,65 @@ no_oa_tables <- c("https://www.nomisweb.co.uk/output/census/2021/census2021-ts00
 # Create output directories for the census tables
 
 dir.create("./output_data/csv",recursive = TRUE)
-dir.create("./output_data/parquet",recursive = TRUE)
+#dir.create("./output_data/parquet",recursive = TRUE)
 
 zip_urls <-  result <- setdiff(zip_urls, no_oa_tables)  # Remove the tables without OA
 
 for (url in zip_urls){
 
-dir.create("./tmp",recursive = TRUE)#create a temporary directory for unzipping
-f <- curl::curl_download(url, tempfile(fileext = ".zip")) # Download the specified zip file
-unzip(f,  exdir="./tmp") # Unzip
-t_tab_loc <- list.files("./tmp", pattern=".*-oa.csv") # Extract the OA csv location
-
-t_name <- unlist(str_split(t_tab_loc,"-"))[2] # Extract the table name
-
-
-assign(t_name,vroom(paste0("./tmp/",t_tab_loc),show_col_types = FALSE) %>% 
-         select(-date,-geography) %>% 
-         column_to_rownames("geography code")) #Move OA code to row names
- 
-old_names <- colnames(get(t_name)) # Get the column names
-new_names <- paste0(t_name, sprintf("%04d",seq_along(all_of(old_names))))  # Create some new column names with zero padding
-
-# Create a list of the new and old names, plus Table ID
-N_list <- list(
-  old_names = old_names,
-  new_names = new_names,
-  Table_ID = t_name
-)
-
-# Creates the meta data table
-N_list <- as_tibble(N_list)
-
-
-# Keep the metadata
-meta_data_table %<>%
-  bind_rows(N_list)
-
-# Change the column names for the data frame
-env <- environment() # Have to be explicit about the env, as  %>% uses a temp environment
-get(t_name) %>%
-  rename_at(vars(old_names), ~new_names) %>%
-  rownames_to_column("OA") %>%
-  assign(t_name,., envir = env) # add a reference to the environment
-
-# Write csv and parquet to the output folders
-write_parquet(get(t_name), paste0("./output_data/parquet/",t_name,".parquet"))
-write_csv(get(t_name), paste0("./output_data/csv/",t_name,".csv"))
-
-#Remove tmp objects
-rm(N_list, old_names, new_names, t_name, t_tab_loc)
-
-#Remove all downloaded files for this table
-unlink("./tmp",recursive = TRUE)
-
+  dir.create("./tmp",recursive = TRUE)#create a temporary directory for unzipping
+  f <- curl::curl_download(url, tempfile(fileext = ".zip")) # Download the specified zip file
+  unzip(f,  exdir="./tmp") # Unzip
+  
+  t_name <- regmatches(url, regexpr("ts\\d{3}[a-z]?", url))
+  
+  t_tab_loc <- list.files("./tmp", pattern=paste(".", t_name, "-ltla.csv", sep = "")) # Extract the LTLA csv location
+  
+  if (length(t_tab_loc) == 0){
+    t_tab_loc <- list.files("./tmp", pattern=paste(".", t_name, "-llta.csv", sep = "")) #ts004 has a typo
+  }
+  
+  #t_name <- unlist(str_split(t_tab_loc,"-"))[2] # Extract the table name
+  
+  
+  assign(t_name,vroom(paste0("./tmp/",t_tab_loc),show_col_types = FALSE) %>% 
+           select(-date,-geography) %>% 
+           column_to_rownames("geography code")) #Move OA code to row names
+   
+  old_names <- colnames(get(t_name)) # Get the column names
+  new_names <- paste0(t_name, sprintf("%04d",seq_along(all_of(old_names))))  # Create some new column names with zero padding
+  
+  # Create a list of the new and old names, plus Table ID
+  N_list <- list(
+    old_names = old_names,
+    new_names = new_names,
+    Table_ID = t_name
+  )
+  
+  # Creates the meta data table
+  N_list <- as_tibble(N_list)
+  
+  
+  # Keep the metadata
+  meta_data_table %<>%
+    bind_rows(N_list)
+  
+  # Change the column names for the data frame
+  env <- environment() # Have to be explicit about the env, as  %>% uses a temp environment
+  get(t_name) %>%
+    rename_at(vars(old_names), ~new_names) %>%
+    rownames_to_column("LTLA") %>%
+    assign(t_name,., envir = env) # add a reference to the environment
+  
+  # Write csv and parquet to the output folders
+  #write_parquet(get(t_name), paste0("./output_data/parquet/",t_name,".parquet"))
+  write_csv(get(t_name), paste0("./output_data/csv/",t_name,".csv"))
+  
+  #Remove tmp objects
+  rm(N_list, old_names, new_names, t_name, t_tab_loc)
+  
+  #Remove all downloaded files for this table
+  unlink("./tmp",recursive = TRUE)
 
 }
 
@@ -131,5 +137,3 @@ meta_data_table2 <- meta_data_table %>%
   mutate(Variable_Name = str_replace_all(Variable_Name, paste0(Table_Name,": "), ""))
 
 write_csv(meta_data_table2, "Table_Metadata.csv")
-
-  
