f673ac74638f3c646ded2f28b35f482782da2e83
Parker <sarah.parker@ons.gov.uk>
2025-05-14 14:22:11 +0100

    Fixed rest of script, amended annotations

--- Downloading_data/standard_illness_ratio_EW.py
+++ Downloading_data/standard_illness_ratio_EW.py
@@ -12,8 +12,8 @@
 
 import pandas as pd
 
-# Read the Excel file containing the disability data from disabilitycensus2021.xlsx from the link in line 4, skips the first 4 rows
-ill_prop_2021 = pd.read_excel("C:/Users/phills1/Office for National Statistics/Geospatial - LAD_data_downloaded/EW_LAD/disabilitycensus2021.xlsx", sheet_name="Table 6", skiprows=4)
+# Read the Excel file containing the disability data from disabilitycensus2021.xlsx from the link in line 4, skips the first 4 rows (removes metadata. data starts on row 5)
+ill_prop_2021 = pd.read_excel("D:/Repos/Area_Classification/disabilitycensus2021.xlsx", sheet_name="Table 6", skiprows=4)
 #print(ill_prop_2021.head())
 
 # renaming columns using underscores instead of spaces
@@ -26,8 +26,8 @@ ill_prop_2021.rename(columns={
 #print(ill_prop_2021.columns)
 
 # Filter and manipulate data
-# selecting to include only 'persons' (not split by male/female), the 2-category for disability
-# and to include only those who are Disabled
+# selecting to include only 'persons' (not split by male/female), the 2-category for Category,
+# and Status is 'Disabled'
 ill_prop_2021 = ill_prop_2021[
     (ill_prop_2021["Sex"] == "Persons") &
     (ill_prop_2021["Category"] == "Two category") &
@@ -44,61 +44,65 @@ ill_prop_2021["sir_age_band"] = ill_prop_2021["Age"].apply(
 
 #print(ill_prop_2021.head() )
 
-# deleting unnecessary columns
+# deleting unnecessary columns (those not need for aggregation steps)
 ill_prop_2021 = ill_prop_2021.drop(columns=[
-    "Year", "Area_Code", "Notes", 
+    "Year", "Local Authority", "Notes", 
     "Lower 95% Confidence Interval", 
-    "Upper 95% Confidence Interval", "Category"
+    "Upper 95% Confidence Interval", "Category", "Age_specific_perc"
 ])
 #print(ill_prop_2021.head() )
 
-# this line trying to filter out the rows where there is a string ([c]) instead of a number
-ill_prop_2021 = type(ill_prop_2021['Count']) != int 
+# filter out the rows where there is a string ([c]) instead of a number
+ill_prop_2021 = ill_prop_2021[pd.to_numeric(ill_prop_2021['Count'], errors='coerce').notnull()]
 
-# aggregating the data to create count and sum of the data by 'Disability_status' and 'sir_age_band'
-ill_prop_2021 = ill_prop_2021.groupby(["Disability_status", "sir_age_band"]).agg(
+# converting the columns to numeric (so next aggregation step works)
+ill_prop_2021['Count'] = pd.to_numeric(ill_prop_2021['Count'], errors='coerce')
+ill_prop_2021['Population'] = pd.to_numeric(ill_prop_2021['Count'], errors='coerce')
+
+# aggregating the data to create count and sum of the data by 'Area_Code' and 'sir_age_band'
+ill_prop_2021 = ill_prop_2021.groupby(["Area_Code", "sir_age_band"]).agg(
     count=("Count", "sum"),
     population=("Population", "sum")
 ).reset_index()
 
 print(ill_prop_2021.head() )
 
+# Create new ill_prop column
 ill_prop_2021["ill_prop"] = ill_prop_2021["count"] / ill_prop_2021["population"]
 
 # Process data sets
-for dat in ["Census_2021_common_var", "Census_2021_oa_changed_common_var"]:
-    tab2 = globals()[dat].copy()
-    tab2["tot_0_14_65_over"] = (
-        tab2["NM_2020_1_1"] + tab2["NM_2020_1_2"] + tab2["NM_2020_1_3"] +
-        tab2["NM_2020_1_14"] + tab2["NM_2020_1_15"] + tab2["NM_2020_1_16"] +
-        tab2["NM_2020_1_17"] + tab2["NM_2020_1_18"]
-    )
-    tab2["tot_15_64"] = tab2["NM_2020_1_0"] - tab2["tot_0_14_65_over"]
-    tab2["Disabled"] = tab2["NM_2056_1_1"] + tab2["NM_2056_1_2"]
-    tab2 = tab2[["Geography_Code", "NM_2020_1_0", "tot_0_14_65_over", "tot_15_64", "Disabled"]]
+# for dat in ["Census_2021_common_var", "Census_2021_oa_changed_common_var"]:
+#     tab2 = globals()[dat].copy()
+#     tab2["tot_0_14_65_over"] = (
+#         tab2["NM_2020_1_1"] + tab2["NM_2020_1_2"] + tab2["NM_2020_1_3"] +
+#         tab2["NM_2020_1_14"] + tab2["NM_2020_1_15"] + tab2["NM_2020_1_16"] +
+#         tab2["NM_2020_1_17"] + tab2["NM_2020_1_18"]
+#     )
+#     tab2["tot_15_64"] = tab2["NM_2020_1_0"] - tab2["tot_0_14_65_over"]
+#     tab2["Disabled"] = tab2["NM_2056_1_1"] + tab2["NM_2056_1_2"]
+#     tab2 = tab2[["Geography_Code", "NM_2020_1_0", "tot_0_14_65_over", "tot_15_64", "Disabled"]]
 
-    # Calculate SIR values
-    # people within the SIR band (0-14 and 65+) with a disability divided by the total number of people in total population with a disability
-    # times 100 to get a percentage
-    ill_prop_0_14_65_over = ill_prop_2021.loc[ill_prop_2021["sir_age_band"] == "0_14_65_over", "ill_prop"].values[0]
-    ill_prop_15_64 = ill_prop_2021.loc[ill_prop_2021["sir_age_band"] == "15_64", "ill_prop"].values[0]
+#     # Calculate SIR values
+#     # people within the SIR band (0-14 and 65+) with a disability divided by the total number of people in total population with a disability
+#     # times 100 to get a percentage
+#     ill_prop_0_14_65_over = ill_prop_2021.loc[ill_prop_2021["sir_age_band"] == "0_14_65_over", "ill_prop"].values[0]
+#     ill_prop_15_64 = ill_prop_2021.loc[ill_prop_2021["sir_age_band"] == "15_64", "ill_prop"].values[0]
 
-    tab2["exp_ill_0_14_65_over"] = ill_prop_0_14_65_over * tab2["tot_0_14_65_over"]
-    tab2["exp_ill_15_64"] = ill_prop_15_64 * tab2["tot_15_64"]
-    tab2["exp_ill"] = tab2["exp_ill_0_14_65_over"] + tab2["exp_ill_15_64"]
-    tab2["SIR"] = round(tab2["Disabled"] / tab2["exp_ill"] * 100, 4)
+#     tab2["exp_ill_0_14_65_over"] = ill_prop_0_14_65_over * tab2["tot_0_14_65_over"]
+#     tab2["exp_ill_15_64"] = ill_prop_15_64 * tab2["tot_15_64"]
+#     tab2["exp_ill"] = tab2["exp_ill_0_14_65_over"] + tab2["exp_ill_15_64"]
+#     tab2["SIR"] = round(tab2["Disabled"] / tab2["exp_ill"] * 100, 4)
 
-    SIR = tab2[["Geography_Code", "SIR"]]
+#     SIR = tab2[["Geography_Code", "SIR"]]
 
-    # Merge SIR values back into the original data set
-    tab = pd.merge(globals()[dat], SIR, on="Geography_Code", how="outer")
-    globals()[dat] = tab
+#     # Merge SIR values back into the original data set
+#     tab = pd.merge(globals()[dat], SIR, on="Geography_Code", how="outer")
+#     globals()[dat] = tab
 
-    # Additional merging for other variables
-    if dat == "Census_2021_common_var":
-        Census_2021_common_var_prop = pd.merge(Census_2021_common_var_prop, SIR, on="Geography_Code", how="outer")
-    if dat == "Census_2021_oa_changed_common_var":
-        Census_2021_oa_changed_common_var_prop = pd.merge(Census_2021_oa_changed_common_var_prop, SIR, on="Geography_Code", how="outer")
+#     # Additional merging for other variables
+#     if dat == "Census_2021_common_var":
+#         Census_2021_common_var_prop = pd.merge(Census_2021_common_var_prop, SIR, on="Geography_Code", how="outer")
+#     if dat == "Census_2021_oa_changed_common_var":
+#         Census_2021_oa_changed_common_var_prop = pd.merge(Census_2021_oa_changed_common_var_prop, SIR, on="Geography_Code", how="outer")
 
-    # will need to repeat this for Scotland and NI data    
-    '''
+#     # will need to repeat this for Scotland and NI data    
