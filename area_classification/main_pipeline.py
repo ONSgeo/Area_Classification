6c436da452670ee1dcab64947f3fea074aa2f59c
Jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-06-11 10:15:08 +0100

    Clustering refactor (#47)
    * Refactor clustering_example.py
    
    * Updated clustering script to have callable wrapper
    
    * Update main pipeline and function args in clustering_example
    
    * rename clustering script
    
    * update function to take either path to df or loaded pd dataframe
    
    * adding placeholder values to config

--- area_classification/main_pipeline.py
+++ area_classification/main_pipeline.py
@@ -2,9 +2,10 @@
 from area_classification.downloading_data.ew_lad_bulk_download import ew_lad_bulk_download
 from area_classification.downloading_data.ni_lgd_downloading_data import ni_lgd_download_data
 # from area_classification.pre_processing. ADD PREPROCESSING OF SCOT
-from area_classification.pre_processing.pre_processing import pre_processing    
+from area_classification.pre_processing.pre_processing import pre_processing  
+from area_classification.analysis.clustering import clustering_wrapper  
 from area_classification.utilities.load_config import load_config
-# from area_classification.clustering.clustering import clustering
+from area_classification.pre_processing.pre_processing import pre_processing    
 
 
 def main_pipeline():
@@ -26,6 +27,15 @@ def main_pipeline():
     pre_processing(ew_df , ni_df, scot_df, config)
 
     # Step 5: Clustering
+    # This assumes the data is saved locally and then loads during clustering. 
+    # Will need to refactor to allow this to take df input.
+    clustering_output = clustering_wrapper(
+        input_dataframe_or_filepath= config["input_data__filepath"],
+        num_clusters=config["number_of_clusters"],
+        n_init=config["number_of_times_k_means_initialised"],
+        output_directory=config["output_directory"],
+        plot_directory=config["plot_directory"],
+        random_seed=config["random_seed"],)
 
     # Step 6: Create outputs
 
