8b22eae59f1b3e74b1cb8e55ccac78960c63445d
ella-goodman <ella.goodman@ons.gov.uk>
2025-06-19 12:29:48 +0100

    Moved config from Aggregating_vairables_EW to its own yaml config, moved function used in that script to pre_processing, updated function in Aggregating_variables to run through the dataframe rather than CSVs in a folder.

--- area_classification/pre_processing/pre_processing.py
+++ area_classification/pre_processing/pre_processing.py
@@ -1,6 +1,15 @@
+import pandas as pd
+import yaml
+
+#not sure if this yaml stuff goes here?
+with open('area_classification/aggregation_setup.yaml', 'r') as file:
+    content = file.read()
+    ew_file_configs = yaml.safe_load(content)
+file_configs = ew_file_configs['ew_file_configs']
 # from area_classification.pre_processing.SIR import SIR
 # from area_classification.pre_processing.convert_to_percentages import convert_to_percentages
 # from area_classification.pre_processing.aggregate_variables import aggregate_variables
+from area_classification.pre_processing.Aggregating_variables import batch_ag_columns
 from area_classification.pre_processing.select_variables import select_variables
 from area_classification.pre_processing.combine_tables import combine_table
 
@@ -10,6 +19,8 @@ def pre_processing(ew_df, ni_df, scot_df, config):
 
     for df in [ew_df, ni_df, scot_df]:
         df_temp = SIR(df)
+        # currently file_configs is just for EW, needs NI and Scot variations also
+        df_temp = batch_ag_columns(df_temp, file_configs)
         df_temp = aggregate_variables(df_temp) #legacy version was aggregate_columns
         if config["count or percent"] == "count":
             # count or percent means we have downloaded the data as a count or a percent
