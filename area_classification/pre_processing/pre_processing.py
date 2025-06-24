85ae1bdc0ad43deae5b62320054f3c81446b9e4e
EllaG <134502757+ella-goodman@users.noreply.github.com>
2025-06-24 17:30:47 +0100

    Updated selecting columns function to work on a singular dataframe (#76)

--- area_classification/pre_processing/pre_processing.py
+++ area_classification/pre_processing/pre_processing.py
@@ -6,6 +6,8 @@ from area_classification.pre_processing.convert_to_percentages import convert_to
 from area_classification.pre_processing.Aggregating_variables import batch_ag_columns
 from area_classification.pre_processing.select_variables import select_variables
 from area_classification.pre_processing.combine_tables import combine_table
+select_variables_lookup = "area_classification/pre_processing/EW_selected_codes_lookup.csv"
+
 
 #Assume that the data has been loaded and is in a pandas dataframe (e.g. ran NI / EW bulks and downloaded Scot)
 def pre_processing(ew_df, ni_df, scot_df, config):
@@ -32,7 +34,8 @@ def pre_processing(ew_df, ni_df, scot_df, config):
         
         #Select the 60 variables a used in previous itterations of the area classification
         df_temp = select_variables(df_temp)
-        dfs[key] = df_temp
+   
+  # need to find a way to overwrite ew_df, ni_df, scot_df with the processed data
 
     # Update and overwrite the dataframes
     #ew_df = dfs["england_wales"]
