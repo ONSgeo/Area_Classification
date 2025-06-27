12e4a8857db0d577feab06846ce44d25e27abf18
jday7879 <57715292+Jday7879@users.noreply.github.com>
2025-06-27 13:09:07 +0100

    loading scotland data

--- area_classification/pre_processing/standard_illness_ratio.py
+++ area_classification/pre_processing/standard_illness_ratio.py
@@ -17,7 +17,7 @@ def sir_processing(config):
     # disability files by age -> sharepoint?
     ew_disability_df = pd.read_csv(config["output_directory"]+config["england_wales_disability_file"]).rename(columns={'Area Code': 'Area_Code', 'Local Authority': 'Local_Authority'})
     ni_disability_df = pd.read_csv(config["output_directory"]+config["ni_disability_file"]).rename(columns={'lgd_code': 'Area_Code', 'lgd': 'Local_Authority'})
-    scotland_disability_df = None#pd.read_csv(config[]).rename(columns={'council_area': 'Area_Code', 'council_area_code': 'Local_Authority'})
+    scotland_disability_df = pd.read_csv(config["output_directory"]+config["scotland_disability_file"]).rename(columns={'council_area': 'Area_Code'})
     combined_disability_df = pd.concat(
         [ew_disability_df, ni_disability_df, scotland_disability_df],)
     # Scotland excluded because it doesnt have council area codes 
@@ -73,6 +73,7 @@ def sir_qa_checks(df: pd.DataFrame, config: dict) -> None:
         None
     """
     # Check if disability_count is int
+    
     assert df['disability_count'].dtype == 'int64', "Disability count should be of type int64"
 
     # Check expected spatial distribution
