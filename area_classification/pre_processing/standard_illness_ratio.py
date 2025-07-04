c2c405e29947679e6a82785953083b5058b581cf
ella-goodman <ella.goodman@ons.gov.uk>
2025-07-04 16:41:41 +0100

    Amending file paths

--- area_classification/pre_processing/standard_illness_ratio.py
+++ area_classification/pre_processing/standard_illness_ratio.py
@@ -37,12 +37,12 @@ def sir_processing(config):
 
     if config["england_wales_disability_file"] in missing_files:
         print(f"Warning: The file {config['england_wales_disability_file']} was not found in the input directory.")
-        convert_disability_age_group_england_wales(config["ew_input_folder"] + config["england_wales_disability_input"], config)
+        convert_disability_age_group_england_wales(config["input_data_directory"] + config["england_wales_disability_input"], config)
     if config["ni_disability_file"] in missing_files:
         print(f"Warning: The file {config['ni_disability_file']} was not found in the input directory.")
-        convert_disability_age_group_northern_ireland(config["ni_input_folder"] + config["ni_disability_input"], config)
+        convert_disability_age_group_northern_ireland(config["input_data_directory"] + config["ni_disability_input"], config)
     if config["scotland_disability_file"] in missing_files:
-        convert_disability_age_group_scotland(config["scot_input_folder"] + config["scotland_disability_input"], config)
+        convert_disability_age_group_scotland(config["input_data_directory"] + config["scotland_disability_input"], config)
         print(f"Warning: The file {config['scotland_disability_file']} was not found in the input directory.")        
 
 
