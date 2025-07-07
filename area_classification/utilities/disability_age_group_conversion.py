a7e8d6efae2e8691f1dd865b6aa215d01e25d685
Parker <sarah.parker@ons.gov.uk>
2025-07-07 17:21:07 +0100

    fixing some more small bugs

--- area_classification/utilities/disability_age_group_conversion.py
+++ area_classification/utilities/disability_age_group_conversion.py
@@ -105,6 +105,9 @@ def convert_disability_age_group_scotland(filepath:str, config: dict) -> pd.Data
     # Replace council area names with LAD codes
     result_df["CA19"] = result_df["CA19"].str.strip().str.lower().map(lookup_dict).fillna(result_df["CA19"])
 
+    output_path = Path(config["input_data_directory"]) / "scot_disability_age_group.csv"
+    result_df.to_csv(output_path, index=False)
+
 	# Save to data QA folder
 	#output_file_path = user_config["qa_folder_path"] + "Scot_disability_input.csv"
     #result_df.to_csv(output_file_path, index=False)
