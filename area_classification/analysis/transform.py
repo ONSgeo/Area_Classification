3d32be77728324d38677e892be4c680a109831ed
ella-goodman <ella.goodman@ons.gov.uk>
2025-07-10 17:06:13 +0100

    DocStrings updated inline with numpy

--- area_classification/analysis/transform.py
+++ area_classification/analysis/transform.py
@@ -7,11 +7,15 @@ def transform_and_standardize_data(df):
     1. Apply the inverse hyperbolic sine (arcsinh) transformation to reduce skewness.
     2. Perform min-max scaling to normalize the data to a range of [0, 1].
     
-    Args:
-        df (pd.DataFrame): Input dataframe with numerical data to transform.
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input dataframe with numerical data to transform.
     
-    Returns:
-        pd.DataFrame: Transformed and standardized dataframe.
+    Returns
+    -------
+    pd.DataFrame
+        Transformed and standardized dataframe.
     """
     df = np.arcsinh(df) # Apply inverse hyperbolic sine transformation
     df = (df - df.min()) / (df.max() - df.min()) # Apply min-max scaling
