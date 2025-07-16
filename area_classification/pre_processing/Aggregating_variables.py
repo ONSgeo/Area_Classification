77d69a4fc97c4801de0cb3593d3414d035761d38
EllaG <134502757+ella-goodman@users.noreply.github.com>
2025-07-16 12:31:39 +0100

    Updated aggregating function to name each file based on the country (#162)
    * Updated aggregating function to name each file based on the country
    
    * tidy up

--- area_classification/pre_processing/Aggregating_variables.py
+++ area_classification/pre_processing/Aggregating_variables.py
@@ -35,11 +35,19 @@ def batch_ag_columns(df_temp, file_configs, user_config):
         df_temp[new_col_name] = df_temp[col_names].sum(axis=1)
         print(f"Added new column '{new_col_name}' to the DataFrame.")
 
+    # Extract the header of column 1 (the country area code type e.g. LTLA, LGD or CA19)
+    if not df_temp.empty:
+        # Convert to string for use in the file name
+        country_lad_code = str(df_temp.columns[0])  
+    else:
+        # Handle empty DataFrame case
+        country_lad_code = "N/A" 
+
     # Ensure QA directory exists
     os.makedirs(os.path.dirname(user_config["qa_folder_path"]), exist_ok=True)
 
-    # Save to data QA folder
-    output_file_path = user_config["qa_folder_path"] + "aggregated_variables_output.csv"
+    # Save to data QA folder with country area code type in the file name
+    output_file_path = f"{user_config['qa_folder_path']}aggregated_variables_output_{country_lad_code}.csv"
     df_temp.to_csv(output_file_path, index=False)
         
     return df_temp
